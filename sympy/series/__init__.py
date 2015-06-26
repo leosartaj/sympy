df5661ece343818a002d60795d3ee954803d2476
Sartaj Singh <singhsartaj94@gmail.com>
2015-06-26 16:48:36 +0530

    Implemented Rational algorithm for computing fps
    Also implemented basic version of FormalPowerSeries

--- sympy/series/__init__.py
+++ sympy/series/__init__.py
@@ -8,9 +8,10 @@ from .residues import residue
 from .sequences import (EmptySequence, SeqPer, SeqFormula, sequence, SeqAdd,
                         SeqMul)
 from .fourier import FourierSeries, fourier_series
+from .formal import fps
 
 O = Order
 
 __all__ = ['Order', 'O', 'limit', 'Limit', 'gruntz', 'series', 'residue',
            'EmptySequence', 'SeqPer', 'SeqFormula', 'sequence',
-           'SeqAdd', 'SeqMul', 'FourierSeries', 'fourier_series']
+           'SeqAdd', 'SeqMul', 'FourierSeries', 'fourier_series', 'fps']
